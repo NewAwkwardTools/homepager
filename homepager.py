ab0dd35402619731a6da2fa43c46c9f69f583ebe
WeepingDogel <weepingdogel@gmail.com>
2021-07-31 14:19:05 +0800

    Fix format

--- homepager.py
+++ homepager.py
@@ -5,11 +5,13 @@ import os
 import argparse
 import toml
 
+
 class Information:
     '''
     This class contains the functions which are used to show 
     something about the program and the helpful information.
     '''
+
     def ShowLogo(self):
         print('''
 ╔╗─╔╗────────╔═══╗
@@ -23,42 +25,51 @@ class Information:
         ''')
         print('v 1.0 beta')
         print('By WeepingDogel')
+
     def GetParser(self):
         '''
         Options
         Here is the place to store the options of the program.
         '''
-        parser = argparse.ArgumentParser(description="A simple generator of homepage.")
-        parser.add_argument('-i','--input', help='The input txt file.',required=True)
+        parser = argparse.ArgumentParser(
+            description="A simple generator of homepage.")
+        parser.add_argument(
+            '-i', '--input', help='The input txt file.', required=True)
         return parser
-    def LoadToml(self,filename):
+
+    def LoadToml(self, filename):
         fileobj = open(filename)
         config = toml.load(fileobj)
         return config
+
+
 class Execution:
     def StepOne(self, txt):
         # The step one, use titlegetter generate a html file from the txt.
         if os.path.exists('public') == False:
-            os.system('mkdir public')
+            os.mkdir('public')
         if os.path.exists('temp') == False:
-            os.system('mkdir temp')
-        os.system('titlegetter -b -i '+ txt + ' -f html -o temp/temp.html')
-    def StepTwo(self,html,title,bio):
+            os.mkdir('temp')
+        os.system('titlegetter -b -i ' + txt + ' -f html -o temp/temp.html')
+
+    def StepTwo(self, html, title, bio):
         # Copy the content of the html file, and write into a other html file.
         # Try to make a page.
-        if os.path.exists("/usr/share/homepager/style.css") == True:
+        if os.path.exists("/usr/share/homepager/style.css"):
             os.system('cp /usr/share/homepager/style.css public/style.css')
         else:
             os.system('cp resources/style.css public/style.css')
-        print(' File "style.css" saved as:' + os.getcwd() + '/public/style.css')
-        page = open('public/index.html','w')
+        print(' File "style.css" saved as:' +
+              os.getcwd() + '/public/style.css')
+        page = open('public/index.html', 'w')
         links = open('temp/temp.html')
         page.write('<!DOCTYPE html>\n')
         page.write('<html>\n')
         page.write('<head>\n')
         page.write('<meta charset="utf-8"/>\n')
         page.write('<link rel="stylesheet" type="text/css" href="style.css">\n')
-        page.write('<meta name="viewport" content="width=device-width, initial-scale=1.0">\n')
+        page.write(
+            '<meta name="viewport" content="width=device-width, initial-scale=1.0">\n')
         page.write('<title>' + title + '</title>\n')
         page.write('</head>\n')
         page.write('<body>\n')
@@ -76,8 +87,11 @@ class Execution:
         page.write('</body>\n')
         page.write('</html>\n')
         page.close()
-        print(' File "index.html" saved as:' + os.getcwd() + '/public/index.html')
+        print(' File "index.html" saved as:' +
+              os.getcwd() + '/public/index.html')
         os.system('rm -rfv temp')
+
+
 def main():
     '''
 The main function, all the things will be begun here.
@@ -88,11 +102,15 @@ The main function, all the things will be begun here.
     parser = Info.GetParser()
     args = parser.parse_args()
     if os.path.exists(str(os.getenv('XDG_CONFIG_HOME' + '/homepager/config.toml'))) == True:
-        config = Info.LoadToml(os.getenv('XDG_CONFIG_HOME' + '/homepager/config.toml'))
+        config = Info.LoadToml(
+            os.getenv('XDG_CONFIG_HOME' + '/homepager/config.toml'))
     elif os.path.exists('/etc/homepager/config.toml') == True:
         config = Info.LoadToml('/etc/homepager/config.toml')
     else:
         config = Info.LoadToml('config/config.toml')
     Exe.StepOne(args.input)
-    Exe.StepTwo('temp/temp.html',config['Main']['TopTitle'],config['Main']['Bio'])
+    Exe.StepTwo('temp/temp.html', config['Main']
+                ['TopTitle'], config['Main']['Bio'])
+
+
 main()
