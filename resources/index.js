fa7e73262e11205cb0905c2e57e9cdb92732e1c8
weepingdogel <weepingdogel@gmail.com>
2021-08-10 15:50:56 +0800

    更正格式

--- resources/index.js
+++ resources/index.js
@@ -1,10 +1,10 @@
 function Search(){
-		// Get the content users wanna search
-		var content = document.forms['form']['search'].value;
-		if(content == '' || content ==null){
-				alert('搜索内容不得为空')
-		}
-		else{
-				window.open("https://www.google.com/search?q=" + content);
-		}
+    // Get the content users wanna search
+    var content = document.forms['form']['search'].value;
+    if(content == '' || content ==null){
+	alert('搜索内容不得为空')
+    }
+    else{
+	window.open("https://www.google.com/search?q=" + content);
+    }
 }
